7a75353555318824a09ae1f2520a3a2f7a2fa393
Alex-afa <alexispratamabahar2006@gmail.com>
2024-10-02 14:39:23 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,11 @@
 using namespace std;
 int generateAngkaRandom() {
 
+srand(time(0));
+    return rand() % 100 + 1;
+}
+
+bool cekJawaban(int tebakan, int angkaAsli) {
+    if (tebakan == angkaAsli) {
+        cout << "Selamat, Anda benar!" << endl;
+        return true;
