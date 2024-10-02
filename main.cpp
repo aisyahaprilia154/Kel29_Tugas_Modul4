9bc67510a2c53595da1d93c1da507176e9643446
aisyahaprilia154 <aisyahaprilia1515@gmail.com>
2024-10-02 14:15:06 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int generateAngkaRandom() {
+    srand(time(0));
+    return rand() % 100 + 1;
+}
+bool cekJawaban(int tebakan, int angkaAsli) {
+    if (tebakan == angkaAsli) {
+        cout << "Selamat, Anda benar!" << endl;
+        return true;
+    } else if (tebakan < angkaAsli) {
+        cout << "Terlalu kecil!" << endl;
+    } else {
+        cout << "Terlalu besar!" << endl;
+    }
+    return false;
+}
+
+void mulaiPermainan() {
+    cout << "Selamat datang di permainan tebak angka!" << endl;
+    cout << "Tebak angka antara 1 sampai 100." << endl;
+
+    int angkaAsli = generateAngkaRandom();
+    int tebakan;
+
+    do {
+        cout << "Masukkan tebakan Anda: ";
+        cin >> tebakan;
+    } while (!cekJawaban(tebakan, angkaAsli));
+}
+
+int main() {
+    mulaiPermainan();
+    return 0;
+}
